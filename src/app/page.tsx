ae525f3e5d470d0f3631228e1542375250570ab3
soongensayo <e0959972@u.nus.edu>
2024-01-14 22:28:50 +0800

    fifth commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,16 @@ import Link from 'next/link'
 import { Button, buttonVariants } from '@/components/ui/button'
 import { ArrowRight } from 'lucide-react'
 import VideoPlayerComponent from '../../player'
+import Head from 'next/head'
 
 export default function Home() {
   return (
     <>
+
+      <Head>
+        <title>Pastures</title>
+        <link rel="icon" href="/pastureslogo1.png"/>
+      </Head>
 
       <MaxWidthWrapper className="mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center">
         <div className='max-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50'>
