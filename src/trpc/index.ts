d50d642688ee277297577d8df7ac614f9988ab36
soongensayo <e0959972@u.nus.edu>
2024-01-14 18:19:01 +0800

    fourth commit

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,7 +1,6 @@
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 import { privateProcedure, publicProcedure, router } from './trpc';
 import { TRPCError } from '@trpc/server';
-import { db } from '@/db';
 import { z } from 'zod'
 
 export const appRouter = router({
@@ -17,22 +16,8 @@ export const appRouter = router({
       throw new TRPCError({ code: 'UNAUTHORIZED' });
     }
 
-    //check if user is in the database
-    const dbUser = await db.user.findFirst({
-      where: {
-        id: user.id
-      }
-    })
-
-    if (!dbUser) {
-      //create user in db
-      await db.user.create({
-        data: {
-          id: user.id,
-          email: user.email
-        }
-      })
-    }
+    
+    
 
     return { success: true };
   }),
@@ -40,35 +25,9 @@ export const appRouter = router({
   getUserFiles: privateProcedure.query(async ({ ctx }) => {
     const { userId } = ctx
 
-    return await db.telebot.findMany({
-      where: {
-        userId,
-      },
-    })
+    
   }),
 
-  deleteFile: privateProcedure.input(
-    z.object({ id: z.string() })
-  ).mutation(async ({ctx, input}) => {
-    const {userId} = ctx
-
-    const telebot = await db.telebot.findFirst({
-      where: {
-        id: input.id,
-        userId,
-      }
-    })
-
-    if(!telebot) throw new TRPCError({code: "NOT_FOUND"})
-
-    await db.telebot.delete({
-      where: {
-        id: input.id,
-      }
-    })
-    
-    return telebot
-  })
 
 });
 
