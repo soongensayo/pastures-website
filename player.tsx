64e9f51fea9ec2b291edfe81b6c021c923602713
soongensayo <e0959972@u.nus.edu>
2024-01-16 15:16:18 +0800

    updated demo video

--- player.tsx
+++ player.tsx
@@ -1,20 +1,19 @@
 'use client'
 
-// Import ReactPlayer
 import ReactPlayer from 'react-player';
 import React from 'react';
-
-
 
 const VideoPlayerComponent: React.FC = () => {
   return (
     <div className='player-wrapper'>
       <ReactPlayer
-        url='https://www.youtube.com/watch?v=RoJPdl7JCEE' // Replace with your video URL
+        url='https://youtu.be/X5zQD-aoFgE' 
         className='react-player'
         playing
         width='100%'
         height='750px'
+        loop
+        playsinline
       />
     </div>
   );
