43702915f3a733c41ce52c5e1e3a8d83fc433bc7
soongensayo <e0959972@u.nus.edu>
2025-02-17 00:09:11 -0500

    update showcase pic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -65,12 +65,12 @@ export default function Home() {
               <div className='mt-16 flow-root sm:mt-24'>
                 <div className='-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4'>
                   <Image
-                    src='/pastures-showcase.png'
+                    src='/pastures-showcase.svg'
                     alt='product preview'
-                    width={1364}
-                    height={866}
+                    width={1920}
+                    height={1080}
                     quality={100}
-                    className='rounded-md bg-white p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-gray-900/10'
+                    className='rounded-md bg-white p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-gray-900/10 w-full h-full object-cover'
                   />
                 </div>
               </div>
